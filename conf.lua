ab6a0d9da9ddcf94a494b73cb27021dfb4f94828
Evan Peterson <evan@evanjp.com>
2025-06-03 20:34:23 +0200

    Love version 11.4 -> 11.5 in conf

--- conf.lua
+++ conf.lua
@@ -2,7 +2,7 @@
 
 function love.conf(t)
     t.title = "Ryba"   -- The title of the window
-    t.version = "11.4" -- The LÖVE version this game was made for
+    t.version = "11.5" -- The LÖVE version this game was made for
     t.window.width = 1024
     t.window.height = 768
     t.window.resizable = false -- Let the window be resizable?
