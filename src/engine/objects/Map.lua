922bb45c22288c2196232da1e16973f7ed5f886f
Evan Peterson <evan@evanjp.com>
2025-06-04 15:03:00 +0200

    Add walls around the map
    PROMPT: Update src/engine/objects/Map.lua to include walls around the edge of the map that the player cannot move past. Map.lua is using the Simple-Tiled-Implementation library to create map objects.

--- src/engine/objects/Map.lua
+++ src/engine/objects/Map.lua
@@ -1,7 +1,6 @@
 Map = Object:extend()
 
 -- TODO: generalize this (for fun)
--- TODO: add walls around the map!
 
 function Map:new(path)
     self.gameMap = sti(path)
@@ -14,6 +13,31 @@ function Map:new(path)
             table.insert(self.walls, wall)
         end
     end
+
+    -- Add edge walls around the map perimeter
+    local mapWidth = self.gameMap.width * self.gameMap.tilewidth
+    local mapHeight = self.gameMap.height * self.gameMap.tileheight
+    local wallThickness = self.gameMap.tilewidth
+
+    -- Top wall
+    local topWall = Game.phys:newRectangleCollider(0, -wallThickness, mapWidth, wallThickness)
+    topWall:setType("static")
+    table.insert(self.walls, topWall)
+
+    -- Bottom wall
+    local bottomWall = Game.phys:newRectangleCollider(0, mapHeight, mapWidth, wallThickness)
+    bottomWall:setType("static")
+    table.insert(self.walls, bottomWall)
+
+    -- Left wall
+    local leftWall = Game.phys:newRectangleCollider(-wallThickness, 0, wallThickness, mapHeight)
+    leftWall:setType("static")
+    table.insert(self.walls, leftWall)
+
+    -- Right wall
+    local rightWall = Game.phys:newRectangleCollider(mapWidth, 0, wallThickness, mapHeight)
+    rightWall:setType("static")
+    table.insert(self.walls, rightWall)
 end
 
 function Map:draw()
